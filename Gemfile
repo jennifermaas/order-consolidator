64801c586f5194c7bcfcb55ed339289c4151e4f0
jennifermaas <jennifermaas@gmail.com>
2018-06-05 18:25:09 +0000

    set initialize_on_precompile to false

--- Gemfile
+++ Gemfile
@@ -25,6 +25,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'fishbowl', '>= 1.0', github: 'zion/fishbowl'
 
+gem 'delayed_job_active_record'
+
+gem 'rails_12factor', group: :production
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,6 @@ module Workspace
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :delayed_job
+    config.assets.initialize_on_precompile = false
   end
 end
