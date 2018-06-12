5c8df10627748130ecf0a5fa3b7c76fe4c8a470e
jennifermaas <jennifermaas@gmail.com>
2018-06-12 02:37:09 +0000

    Searches for customer orders by name and account number.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,6 +10,80 @@ class Customer < ActiveRecord::Base
     scope :needed_consolidation, -> { where(needed_consolidation: true) }
     scope :did_not_need_consolidation, -> { where(needed_consolidation: false) }
     
+    def create_sales_orders
+        builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+            xml.request {
+              xml.GetSOListRq {
+                xml.Status 'All Open'
+                xml.LocationGroupName 'LITA'
+                xml.AccountNumber self.account_number
+                xml.CustomerName self.name
+              }
+            }
+          end
+          code, response = Fishbowl::Objects::BaseObject.new.send_request(builder, "ProductGetRs")
+          sales_order_params = {}
+          response.xpath("FbiXml//SalesOrder").each do |sales_order_xml|
+            unless (sales_order_xml.xpath("PriorityId").inner_html[0]=="5") || (sales_order_xml.xpath("Number").inner_html[0]=="G") || (sales_order_xml.xpath("Status").inner_html=="10") || (sales_order_xml.xpath("Number").inner_html[0]=="R") || (sales_order_xml.xpath("Number").inner_html[0]=="@")
+              sales_order_params["num"]=sales_order_xml.at_xpath("Number").try(:content)
+              sales_order_params["customer_id"]=self.id
+              sales_order_params["customer_contact"]=sales_order_xml.at_xpath("CustomerContact").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+              sales_order_params["bill_to_name"]=sales_order_xml.at_xpath("BillTo//Name").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+              sales_order_params["bill_to_address"]=sales_order_xml.at_xpath("BillTo//AddressField").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+              sales_order_params["bill_to_city"]=sales_order_xml.at_xpath("BillTo//City").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+              sales_order_params["bill_to_state"]=sales_order_xml.at_xpath("BillTo//State").try(:content)
+              sales_order_params["bill_to_zip"]=sales_order_xml.at_xpath("BillTo//Zip").try(:content)
+              sales_order_params["bill_to_country"]=sales_order_xml.at_xpath("BillTo//Country").try(:content)
+              sales_order_params["ship_to_name"]=sales_order_xml.at_xpath("Ship//Name").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+              sales_order_params["ship_to_address"]=sales_order_xml.at_xpath("Ship//AddressField").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+              sales_order_params["ship_to_city"]=sales_order_xml.at_xpath("Ship//City").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+              sales_order_params["ship_to_state"]=sales_order_xml.at_xpath("Ship//State").try(:content)
+              sales_order_params["ship_to_zip"]=sales_order_xml.at_xpath("Ship//Zip").try(:content)
+              sales_order_params["ship_to_country"]=sales_order_xml.at_xpath("Ship//Country").try(:content)
+              sales_order_params["ship_to_residential"]
+              sales_order_params["carrier_name"]=sales_order_xml.at_xpath("Carrier").try(:content)
+              sales_order_params["tax_rate_name"]=sales_order_xml.at_xpath("TaxRateName").try(:content)
+              sales_order_params["priority_id"]=sales_order_xml.at_xpath("PriorityId").try(:content)
+              sales_order_params["po_num"]=sales_order_xml.at_xpath("PoNum").try(:content)
+              sales_order_params["date"]=sales_order_xml.at_xpath("CreatedDate").try(:content)
+              sales_order_params["salesman"]=sales_order_xml.at_xpath("Salesman").try(:content)
+              sales_order_params["shipping_terms"]=sales_order_xml.at_xpath("ShippingTerms").try(:content)
+              sales_order_params["payment_terms"]=sales_order_xml.at_xpath("PaymentTerms").try(:content)
+              sales_order_params["fob"]=sales_order_xml.at_xpath("FOB").try(:content)
+              sales_order_params["note"]=sales_order_xml.at_xpath("Note").try(:content)
+              sales_order_params["quickbooks_class_name"]=sales_order_xml.at_xpath("QuickBooksClassName").try(:content)
+              sales_order_params["location_group_name"]=sales_order_xml.at_xpath("LocationGroup").try(:content)
+              sales_order_params["fulfillment_date"]=sales_order_xml.at_xpath("DateCompleted").try(:content)
+              sales_order_params["url"]=sales_order_xml.at_xpath("URL").try(:content)
+              sales_order_params["carrier_service"]=""
+              sales_order_params["currency_name"]=sales_order_xml.at_xpath("currencyName").try(:content)
+              sales_order_params["currency_rate"]=sales_order_xml.at_xpath("currencyRate").try(:content)
+              sales_order_params["price_is_home_currency"]=sales_order_xml.at_xpath("PriceIsHomeCurrency").try(:content)
+              sales_order_params["date_expired"]=""
+              sales_order_params["phone"]=""
+              sales_order_params["email"]=""
+              sales_order = SalesOrder.create(sales_order_params)
+              sales_order_xml.xpath("Items//SalesOrderItem").each do |sales_order_item_xml|
+                sales_order_item_params={}
+                sales_order_item_params["num"]=sales_order_item_xml.at_xpath("ID").try(:content)
+                sales_order_item_params["sales_order_id"]=sales_order.id
+                sales_order_item_params["product_num"]=sales_order_item_xml.at_xpath("ProductNumber").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+                product=""
+                sales_order_item_params["so_item_type_id"]=sales_order_item_xml.at_xpath("ItemType").try(:content)
+                sales_order_item_params["qty_to_fulfill"]=sales_order_item_xml.at_xpath("Quantity").try(:content)
+                sales_order_item_params["uom"]=sales_order_item_xml.at_xpath("UOMCode").try(:content)
+                sales_order_item_params["product_price"]=sales_order_item_xml.at_xpath("ProductPrice").try(:content)
+                sales_order_item_params["taxable"]=sales_order_item_xml.at_xpath("Taxable").try(:content)
+                sales_order_item_params["note"]=sales_order_item_xml.at_xpath("Note").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+                sales_order_item_params["quickbooks_class_name"]=sales_order_item_xml.at_xpath("QuickBooksClassName").try(:content)
+                sales_order_item_params["show_item"]=sales_order_item_xml.at_xpath("ShowItemFlag").try(:content)
+                sales_order_item_params["revision_level"]=sales_order_item_xml.at_xpath("RevisionLevel").try(:content)
+                sales_order_item=SalesOrderItem.create(sales_order_item_params)
+              end
+            end
+          end
+    end
+    
     def void_orders_in_fishbowl
         self.sales_orders.each do |order|
           return false unless order.void_in_fishbowl
