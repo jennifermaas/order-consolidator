f1cd0c2a6190d14bde5ff7c60ebd1b6df604d929
jennifermaas <jennifermaas@gmail.com>
2018-12-07 05:43:27 +0000

    change_create_orders_to_work_indidually

--- app/models/order_consolidation.rb
+++ app/models/order_consolidation.rb
@@ -16,10 +16,6 @@ class OrderConsolidation < ActiveRecord::Base
         create_inventory
         create_message "Products Found: #{self.products.count}"
         #
-        create_message "Starting Create Customers"
-        create_customers
-        create_message "Customers Found: #{self.customers.count}"
-        #
         create_message "Starting Create Sales Orders"
         create_sales_orders
         #
@@ -247,13 +243,115 @@ class OrderConsolidation < ActiveRecord::Base
         end
     end
     
-    def create_customers
-      Customer.create_from_open_orders(self)
-    end
-    
     def create_sales_orders
-        self.customers.each do |customer|
-          customer.create_sales_orders
+      require 'csv'
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.request {
+          xml. ExecuteQueryRq {
+            xml.Query  "SELECT customer.id,customer.name, customer.number, So.num
+                        FROM So INNER JOIN Customer on So.CustomerId=Customer.id
+                        WHERE  (So.statusId=20)
+                          AND NOT (num LIKE 'G%')
+                          AND NOT (num LIKE 'g%')
+                          AND NOT (num LIKE 'R%')
+                          AND NOT (num LIKE 'r%')
+                          AND NOT (num LIKE '@%')
+                          AND (So.customerId NOT IN (328,1603,333,758,1576,1319,1427,1365)) 
+                          AND NOT (customer.name LIKE '%Alliance%')
+                          AND NOT (customer.name LIKE '%All Media Supply%')
+                          AND NOT (customer.name LIKE '%Baker%')
+                          AND NOT (customer.name LIKE '%PROMO%')
+                          AND NOT (customer.name LIKE '%LITA Store%')
+                          AND NOT (customer.name LIKE '%Cargo%')
+                          AND NOT (customer.name LIKE '%PIAS%')
+                          AND NOT (customer.name LIKE '%Inertia%')
+                          AND NOT (customer.name = 'Revolver')
+                          AND NOT (customer.name LIKE '%Outside%')
+                          AND NOT (customer.name LIKE '%SRD%')
+                          AND NOT (customer.name LIKE '%Border%')
+                          AND NOT (customer.name LIKE '%Southbound%')
+                          AND NOT (customer.name LIKE '%Cobraside%')
+                          AND NOT (customer.name LIKE '%Tsunami%')
+                          AND NOT (customer.name LIKE '%Pop Up Event%')
+                        ORDER BY customer.name"
+          }
+        }
+      end
+      code, response = Fishbowl::Objects::BaseObject.new.send_request(builder, "ProductGetRs")
+      previous_customer_name=""
+      response.xpath("//Row")[1..-1].each do |row|
+        row_array=row.content.parse_csv
+        unless previous_customer_name == row_array[1]
+          customer = Customer.create(fb_id: row_array[0], name: row_array[1], account_number: row_array[2], order_consolidation: order_consolidation)
+          previous_customer_name = row_array[1]
         end
+        builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+          xml.request {
+            xml.LoadSORq {
+              xml.Number "#{row_array[3]}"
+            }
+          }
+        end
+        code, response = Fishbowl::Objects::BaseObject.new.send_request(builder, "GetSOListRq")
+        sales_order_params = {}
+        response.xpath("FbiXml//SalesOrder").each do |sales_order_xml|
+            sales_order_params["num"]=sales_order_xml.at_xpath("Number").try(:content)
+            sales_order_params["customer_id"]=customer.id
+            sales_order_params["customer_contact"]=sales_order_xml.at_xpath("CustomerContact").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+            sales_order_params["bill_to_name"]=sales_order_xml.at_xpath("BillTo//Name").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+            sales_order_params["bill_to_address"]=sales_order_xml.at_xpath("BillTo//AddressField").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+            sales_order_params["bill_to_city"]=sales_order_xml.at_xpath("BillTo//City").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+            sales_order_params["bill_to_state"]=sales_order_xml.at_xpath("BillTo//State").try(:content)
+            sales_order_params["bill_to_zip"]=sales_order_xml.at_xpath("BillTo//Zip").try(:content)
+            sales_order_params["bill_to_country"]=sales_order_xml.at_xpath("BillTo//Country").try(:content)
+            sales_order_params["ship_to_name"]=sales_order_xml.at_xpath("Ship//Name").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+            sales_order_params["ship_to_address"]=sales_order_xml.at_xpath("Ship//AddressField").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+            sales_order_params["ship_to_city"]=sales_order_xml.at_xpath("Ship//City").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+            sales_order_params["ship_to_state"]=sales_order_xml.at_xpath("Ship//State").try(:content)
+            sales_order_params["ship_to_zip"]=sales_order_xml.at_xpath("Ship//Zip").try(:content)
+            sales_order_params["ship_to_country"]=sales_order_xml.at_xpath("Ship//Country").try(:content)
+            sales_order_params["ship_to_residential"]
+            sales_order_params["carrier_name"]=sales_order_xml.at_xpath("Carrier").try(:content)
+            sales_order_params["tax_rate_name"]=sales_order_xml.at_xpath("TaxRateName").try(:content)
+            sales_order_params["priority_id"]=sales_order_xml.at_xpath("PriorityId").try(:content)
+            sales_order_params["po_num"]=sales_order_xml.at_xpath("PoNum").try(:content)
+            sales_order_params["date"]=sales_order_xml.at_xpath("CreatedDate").try(:content)
+            sales_order_params["salesman"]=sales_order_xml.at_xpath("Salesman").try(:content)
+            sales_order_params["shipping_terms"]=sales_order_xml.at_xpath("ShippingTerms").try(:content)
+            sales_order_params["payment_terms"]=sales_order_xml.at_xpath("PaymentTerms").try(:content)
+            sales_order_params["fob"]=sales_order_xml.at_xpath("FOB").try(:content)
+            sales_order_params["note"]=sales_order_xml.at_xpath("Note").try(:content)
+            sales_order_params["quickbooks_class_name"]=sales_order_xml.at_xpath("QuickBooksClassName").try(:content)
+            sales_order_params["location_group_name"]=sales_order_xml.at_xpath("LocationGroup").try(:content)
+            sales_order_params["fulfillment_date"]=sales_order_xml.at_xpath("DateCompleted").try(:content)
+            sales_order_params["url"]=sales_order_xml.at_xpath("URL").try(:content)
+            sales_order_params["carrier_service"]=""
+            sales_order_params["currency_name"]=sales_order_xml.at_xpath("currencyName").try(:content)
+            sales_order_params["currency_rate"]=sales_order_xml.at_xpath("currencyRate").try(:content)
+            sales_order_params["price_is_home_currency"]=sales_order_xml.at_xpath("PriceIsHomeCurrency").try(:content)
+            sales_order_params["date_expired"]=""
+            sales_order_params["phone"]=""
+            sales_order_params["email"]=""
+            sales_order = SalesOrder.create(sales_order_params)
+            sales_order_xml.xpath("Items//SalesOrderItem").each do |sales_order_item_xml|
+              sales_order_item_params={}
+              sales_order_item_params["num"]=sales_order_item_xml.at_xpath("ID").try(:content)
+              sales_order_item_params["sales_order_id"]=sales_order.id
+              sales_order_item_params["product_num"]=sales_order_item_xml.at_xpath("ProductNumber").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+              product=""
+              sales_order_item_params["so_item_type_id"]=sales_order_item_xml.at_xpath("ItemType").try(:content)
+              sales_order_item_params["qty_to_fulfill"]=sales_order_item_xml.at_xpath("Quantity").try(:content)
+              sales_order_item_params["uom"]=sales_order_item_xml.at_xpath("UOMCode").try(:content)
+              sales_order_item_params["product_price"]=sales_order_item_xml.at_xpath("ProductPrice").try(:content)
+              sales_order_item_params["taxable"]=sales_order_item_xml.at_xpath("Taxable").try(:content)
+              sales_order_item_params["note"]=sales_order_item_xml.at_xpath("Note").try(:content).force_encoding('iso-8859-1').encode('utf-8')
+              sales_order_item_params["quickbooks_class_name"]=sales_order_item_xml.at_xpath("QuickBooksClassName").try(:content)
+              sales_order_item_params["show_item"]=sales_order_item_xml.at_xpath("ShowItemFlag").try(:content)
+              sales_order_item_params["revision_level"]=sales_order_item_xml.at_xpath("RevisionLevel").try(:content)
+              sales_order_item_params["kit_item"]=sales_order_item_xml.at_xpath("KitItem").try(:content)
+              sales_order_item=SalesOrderItem.create(sales_order_item_params)
+            end
+        end
+      end
     end
 end
